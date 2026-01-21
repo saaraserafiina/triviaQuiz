d681892aa875db32f30bbd81b8eb8a65cf8d0c88
saaraheikkila <saara.heikkila@icloud.com>
2026-01-21 22:08:09 +0200

    Added category option

--- trivia.py
+++ trivia.py
@@ -1,40 +1,56 @@
 import requests
 import html
 import random
-
-
-#url = "https://opentdb.com/api.php?amount=10"
-
-
-def get_questions():
-
-
-    return
-
-def run_quiz():
-
-        return
-
-
-
+import time
 
 def game_settings():
+
      
     questions_amount = int(input("How many questions would you like to have? (maximum 50) "))
 
     if questions_amount > 50:
          print("Only 50 questions at a time!")
-         return
+    else:
+            url = f"https://opentdb.com/api.php?amount={questions_amount}"
 
-    difficulty = input("Difficulty (easy, medium, hard, any (press enter for many))) ").lower()
+
+
+    cat_url = "https://opentdb.com/api_category.php"
+
+    time.sleep(0.2)
+
+    print("Now let's select a category.")
+    time.sleep(0.2)
+    print()
+    print("These are the categories and the corresponding numbers. Pick one!")
+    print()
+
+    response = requests.get(cat_url)
+    data = response.json()
+
+    categories = data["trivia_categories"]
+
+
+    for category in categories:
+
+        print(f"Number: {category['id']} - Category: {category['name']}")
+        time.sleep(0.2)
+
+    print()
+    selected_category = int(input("For any category, just press 0! "))
+
+    if 9 <= selected_category <=32:
+        url += f"&category={selected_category}"
+
+
+    difficulty = input("Difficulty (easy, medium, hard, any (press enter for any))) ").lower()
+
+    if difficulty in ["easy", "medium", "hard"]:
+        url += f"&difficulty={difficulty}"
+
 
     type = input("Type (multiple, boolean, any (press enter for any))" ).lower()
 
-    url = f"https://opentdb.com/api.php?amount={questions_amount}"
-
-    if difficulty in ["easy", "medium", "hard"]:
-
-        url += f"&difficulty={difficulty}"
     
     if type in ["multiple", "boolean"]:
          url += f"&type={type}"
@@ -46,13 +62,20 @@ def main():
 
     score = 0
     url = game_settings()
+    print(url)
+    
     
     response = requests.get(url)
     data = response.json()
 
+    if data["response_code"]==1:
+         print("Not enough questions for this query. Try fewer questions or another category.")
+         return
+
     question_data = data["results"]
     print()
 
+    
     for i, q in enumerate(question_data, start=1):
          
         question = html.unescape(q["question"])
@@ -61,7 +84,6 @@ def main():
 
         options = [correct] + incorrect
         random.shuffle(options)
-
 
 
         print(question)
@@ -77,7 +99,9 @@ def main():
 
         else:
              print(f"Incorrect answer! The correct answer was {correct}")
-             print()
+             print() 
+    
+    print(f"Your total score was {score} points out of {len(question_data)}")
 
 
 
